e631faa600b1a8911dbe4040e80863c7fef03c22
casperdewith <35010340+casperdewith@users.noreply.github.com>
2022-10-28 08:36:54 +0200

    Balance the frequency of letters
    Changed the letter-generation algorithm to five dozen-sided dice. The problematic letters ‹u›, ‹i› and ‹e› now occur far less often.

--- data.go
+++ data.go
@@ -1,33 +1,83 @@
 package main
 
 import "math/rand"
+// import "fmt"   // for testing
+// import "time"
 
-var letterData = []string{"k", "l", "m", "n", "p", "s", "t", "w", "j", "a", "e", "i", "o", "u", "k", "l", "m", "n", "p", "s", "t", "w", "j", "a", "e", "i", "o", "u", "k", "l", "m", "n", "p", "s", "t", "w", "j", "a", "e", "i", "o", "u"}
-var particles = map[string]struct{}{
+var particles = map[string]struct{} {
 	"li": {},
 	"e":  {},
 	"la": {},
 	"o":  {},
 	"pi": {},
-	"a":  {},
+	"en": {},
 }
 
-func randomLetter() string {
-	return letterData[rand.Intn(len(letterData))]
+// kiwen — lon musi la sitelen ‹.› li kama weka
+
+const nPoka int = 12 // nanpa poka kiwen
+
+var kiwenSuli = [5]string {
+    "eioujklmnpst",
+    "aiojklmnpstw",
+    "alnpst......",
+    "akmpsw......",
+    "klmntw......",
 }
 
-func randomLength() int {
-	return rand.Intn(3) + 2
+var kiwenNamako = [5]string {
+    "aouwljptknms",
+    "aeiwljptknms",
+    "aiouwlptknms",
+    "aiowljptknms",
+    "aeowljptknms",
 }
 
-func randomLetters(tip int) []string {
-	length := randomLength() + tip
-	if length < 2 {
-		length = 2
-	}
-	arr := make([]string, length)
-	for i := 0; i < length; i++ {
-		arr[i] = randomLetter()
-	}
-	return arr
+func randomLetters(nNamako int) []string {
+    // theoretical maximum number of letters
+    nMuSewi := len(kiwenSuli) + nNamako * len(kiwenNamako)
+    
+    // empty array with capacity set at maximum theoretical
+    mu := make([]string, 0, nMuSewi)
+
+    /// o kama jo e mu tan kiwen suli
+    for k := 0; k < len(kiwenSuli); k++ { // k = kiwen
+        rand.Seed(time.Now().UTC().UnixNano())
+        m := string(kiwenSuli[k][rand.Intn(nPoka)]) // roll one die
+        if m != "." { // if not ‹.›: add;
+            mu = append(mu, m)
+        }             // if ‹.›: continue
+    }
+    
+    /// o kama jo e mu tan kiwen namako
+    for t := 0; t < nNamako; t++ { // t = tenpo
+        for k := 0; k < len(kiwenNamako); k++ { // k = kiwen
+            m := string(kiwenNamako[k][rand.Intn(nPoka)]) // roll one die
+            if m != "." { // if not ‹.›: add;
+                mu = append(mu, m)
+            }             // if ‹.›: continue
+        }
+    }
+    
+    /// o nasa e mu
+    nMu := len(mu) // the actual number of letters it has
+    nasinNasa := rand.Perm(nMu) // random shuffle order
+    
+    muNasa := make([]string, nMu)
+    
+    // construct new array by using the random permutation
+    for m := 0; m < nMu; m++ { // m = mu
+        muNasa[m] = mu[nasinNasa[m]]
+    }
+    
+	return muNasa
 }
+
+/* // for testing
+func main() {
+    for j := 0; j < 10; j++ {
+        fmt.Println(randomLetters(0))
+        // o ante e nanpa pi kulupu pi kiwen namako
+    }
+}
+// */
